149cf752182a9222184e0350327bdfbe5ba61b2f
Asciugano <samuele.diaferio@fastwebnet.it>
2025-06-05 17:20:54 +0200

    prima prova

--- src/main/java/com/asciugano/edu_site/controllers/GameController.java
+++ src/main/java/com/asciugano/edu_site/controllers/GameController.java
@@ -1,8 +1,14 @@
 package com.asciugano.edu_site.controllers;
 
+import com.asciugano.edu_site.models.Game;
 import com.asciugano.edu_site.repositories.GameRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class GameController {
@@ -15,11 +21,44 @@ public class GameController {
 
     @GetMapping("/")
     public String index() {
-        return "/info";
+        return "info";
     }
 
     @GetMapping("/info")
     public String info() {
-        return "/info";
+        return "info";
+    }
+
+    @GetMapping("/games")
+    public String play(Model model) {
+        List<Game> games = gameRepository.findAll();
+
+        model.addAttribute("games", games);
+        return "games";
+    }
+
+    @GetMapping("/play/{id}")
+    public String play(@PathVariable Long id, Model model) {
+        Game game = gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+
+        model.addAttribute("game", game);
+
+        return game.getPath();
+    }
+
+    @GetMapping("/game/{id}")
+    public String gameById(@PathVariable Long id, Model model) {
+        Game game = gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+        model.addAttribute("game", game);
+
+        return "game";
+    }
+
+    @GetMapping("/game")
+    public String gameByName(Model model, @RequestParam String name) {
+        Game game = gameRepository.findByTitle(name).orElseThrow(() -> new RuntimeException("Game not found"));
+
+        model.addAttribute("game", game);
+        return game.getPath();
     }
 }
--- src/main/java/com/asciugano/edu_site/repositories/GameRepository.java
+++ src/main/java/com/asciugano/edu_site/repositories/GameRepository.java
@@ -3,5 +3,8 @@ package com.asciugano.edu_site.repositories;
 import com.asciugano.edu_site.models.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GameRepository extends JpaRepository<Game, Long> {
+    Optional<Game> findByTitle(String name);
 }
