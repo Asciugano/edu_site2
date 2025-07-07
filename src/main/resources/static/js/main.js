4a77b8b3f3bdaf3d82554e87570bac4bc4954f62
Asciugano <samuele.diaferio@fastwebnet.it>
2025-07-07 12:05:53 +0200

    provato ad aggiungere il pulsante per le impostazioni
    pulsante funzionante, menu no

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,52 @@
+let lightTheme = true;
+
 document.querySelector('.title').addEventListener('click', () => {
   window.location.href = '/';
-})
+});
+
+function openSettings() {
+  const existingMenu = document.querySelector('#settings-menu');
+  if (existingMenu) {
+    existingMenu.remove();
+    return;
+  }
+
+  const menu = document.createElement('div');
+  menu.classList.add('settings-menu');
+  menu.id = 'settings-menu';
+
+  const label = document.createElement('label');
+  label.textContent = 'Tema: ';
+
+  const btn = document.createElement('button');
+  btn.onclick = changeTheme;
+
+  const icon = document.createElement('img')
+  icon.src = lightTheme ? '../static/icon/light.png' : '../static/icon/light.png';
+
+  btn.appendChild(icon)
+
+  menu.appendChild(label);
+  menu.appendChild(btn);
+}
+
+function changeTheme(theme = null) {
+  document.body.classList.remove('theme-light', 'theme-dark');
+  if (!theme) {
+    lightTheme = !lightTheme;
+    document.body.classList.add(lightTheme ? 'theme-light' : 'theme-dark');
+
+    localStorage.setItem('theme', lightTheme ? 'theme-light' : 'theme-dark');
+  }
+  else {
+    lightTheme = theme === 'theme-light';
+    document.body.classList.add(theme);
+
+    localStorage.setItem('theme', theme);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const theme = localStorage.getItem('theme') || 'theme-light';
+  changeTheme(theme);
+});
